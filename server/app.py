2b564c9ff3e6fb4f9fc052c6a97d603a8a400e18
Felix-Okeyo <felixokeyo20@gmail.com>
2023-10-26 12:27:12 +0300

    logi, register, profile endpoints added

--- server/app.py
+++ server/app.py
@@ -25,10 +25,118 @@ class Home(Resource):
         }
         return make_response(response_message, 200)
 
-
-
 api.add_resource(Home, '/')
 
+class SignUpResource(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('first_name', type=str, required=True)
+        parser.add_argument('last_name', type=str, required=True)
+        parser.add_argument('username', type=str, required=True)
+        parser.add_argument('email', type=str, required=True)
+        parser.add_argument('ph_address', type=str, required=True)
+        parser.add_argument('password', type=str, required=True)
+        parser.add_argument('telephone', type=int, required=True)
+        parser.add_argument('city_town', type=str, required=True)
+        args = parser.parse_args()
+
+        # Check if the username or email already exists in the database
+        if User.query.filter_by(username=args['username']).first() is not None:
+            return {'message': 'Username already exists'}, 400
+        if User.query.filter_by(email=args['email']).first() is not None:
+            return {'message': 'Email already exists'}, 400
+
+        # Create a new User instance and add it to the database
+        new_user = User(
+            first_name=args['first_name'],
+            second_name=args['second_name'],
+            username=args['username'],
+            email=args['email'],
+            ph_address = args['ph_address'],
+            password=args['password'],
+            telephone = args['telephone'],
+            city_town = args['city_town']
+        )
+        db.session.add(new_user)
+        db.session.commit()
+
+        # Generate an access token for the newly registered user
+        access_token = create_access_token(identity=new_user.id)
+
+        return {
+            'message': 'User registered successfully',
+            'access_token': access_token
+        }, 201
+api.add_resource(SignUpResource, '/register')
+       
+#testing the JWT authentication separately
+class TestJWT(Resource):
+    @jwt_required()
+    def get(self):
+        current_user = get_jwt_identity()
+        return {'user_id': current_user}    
+
+api.add_resource(TestJWT, '/testing')
+
+#handle the login requests
+class LoginResource(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('username', type=str, required=True)
+        parser.add_argument('password', type=str, required=True)
+        args = parser.parse_args()
+
+        user = User.query.filter_by(username=args['username']).first()
+
+        if user and user.password == args['password']:
+            access_token = create_access_token(identity=user.id)
+            return {'access_token': access_token}, 200
+        else:
+            return {'message': 'Invalid credentials'}, 401
+api.add_resource(LoginResource, '/login')
+
+#Admin interface for users management
+class ProfileResource(Resource):
+    @jwt_required()
+    def get(self, id):
+        user = User.query.get_or_404(id)
+        if user:
+            user_dict = {
+                "id": user.id,
+                "first_name": user.first_name,
+                "last_name": user.last_name,
+                "username": user.username,
+                "email": user.email,
+                "ph_address": user.ph_address,
+                "password": user.password,
+                "telephone": user.telephone,
+                "city_town": user.city_town   
+            }
+            return make_response(jsonify(user_dict), 200)
+        else:
+            return make_response(jsonify({"error": "User not found"}),404)
+   
+    def put(self, id):
+        user = User.query.get_or_404(id)
+        parser = reqparse.RequestParser()
+        parser.add_argument('username', type=str)
+        parser.add_argument('email', type=str)
+        args = parser.parse_args()
+
+        for key, value in args.items():
+            if value is not None:
+                setattr(user, key, value)
+
+        db.session.commit()
+        return {'message': 'User details updated successfully'}
+
+   
+    def delete(self, id):
+        user = User.query.get_or_404(id)
+        db.session.delete(user)
+        db.session.commit()
+        return {'message': 'User profile deleted successfully'}    
+    
 
 if __name__ == '__main__':
     app.run(port=5555)
