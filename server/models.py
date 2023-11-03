abe6b6831962d6cefbc7f82df176be8fd35540d3
Felix Okeyo <135003630+Felix-Okeyo@users.noreply.github.com>
2023-11-03 11:59:42 +0300

    Development (#19)
    * class user and product
    
    * api up and running
    
    * requirements.txt added
    
    * requirements.txt changed and pip version updated for 2nd deploying attempt
    
    * requirements reinstallation deploy attempt 3
    
    * requirements reinstallation deploy attempt 4 packagin specified
    
    * pipenv change
    
    * markup Safe error
    
    * markup Safe rq error commented out
    
    * pip change
    
    * og req file
    
    * packagin to packaging typo fixed in rqtxt
    
    * brand logo column added to db
    
    * brand logo column added to db
    
    * seed.py initial
    
    * seeded db
    
    * logi, register, profile endpoints added
    
    * products endpoint added
    
    * products and productbyid endpoints
    
    * profileresource error handled
    
    * endpoints up to productsbyid tested and bugs handled
    
    * brands and brandsbyid endpoints
    
    * joint table for invoice products added and db seeded with no errors
    
    * stupid invoices sorted out based on the products per an invoice, joint table added as well
    
    * stupid invoices sorted out based on the products per an invoice, joint table added as well
    
    * invoices by id done, invoices all to be done
    
    * jwt and needed endpoints and methods done awaiting rbac
    
    * roles table added and rbac started
    
    * added new user role table in db
    
    * jwt removed for dev simplicity
    
    * rbac underway
    
    * invoice by id route added
    
    * rbac done and tested
    
    * new requirements.txt added
    
    * gunicorn rqtxt
    
    * done seeded db
    
    * gunicorn added deploy debugged
    
    * faker added deploy debugged
    
    * alembic errror removed deploy debugged
    
    * jsonschema error removed deploy debugged
    
    * pyrsistent error removed deploy debugged
    
    * get all users added
    
    * seeding errors handled, db migrations made, && db seeded

---
+++ server/models.py
@@ -0,0 +1,104 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from datetime import datetime
+from sqlalchemy_serializer import SerializerMixin
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata = metadata)
+
+class Role(db.Model, SerializerMixin):
+    __tablename__ = "roles"
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(50), nullable = False)
+    
+    
+    #Relationships
+    users = db.relationship("User", back_populates = 'roles')
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    ph_address= db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    telephone = db.Column(db.Integer, nullable=False)
+    city_town = db.Column(db.String(100), nullable=False)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'), nullable = False)
+    
+    #relationship
+    invoice = db.relationship('Invoice', back_populates = 'users')
+    roles = db.relationship('Role', back_populates = 'users')
+
+class Product(db.Model, SerializerMixin):
+    __tablename__ = 'products'
+    
+    id = db.Column(db.Integer, primary_key = True)
+    image = db.Column(db.String(255), nullable=False)
+    p_name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Integer, nullable = False)
+    category = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable = False)
+    brand = db.Column(db.Integer, db.ForeignKey('brands.id'), nullable = False)
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+    
+    #relationships
+    categories = db.relationship('Category', back_populates = 'product')
+    brands = db.relationship('Brand', back_populates = 'product')
+    invoice = db.relationship('Invoice', back_populates = 'products')
+    invoice_products = db.relationship('InvoiceProducts', back_populates = 'product_rl')
+   
+class Category (db.Model, SerializerMixin):
+    __tablename__ = 'categories'
+    
+    id = db.Column(db.Integer, primary_key = True)
+    cat_name = db.Column(db.String(150), nullable =False)
+    
+    #relationships
+    product = db.relationship('Product', back_populates = 'categories')
+    
+class Brand (db.Model, SerializerMixin):
+    __tablename__ = 'brands'
+    
+    id = db.Column(db.Integer, primary_key = True)
+    brand_name = db.Column(db.String(150), nullable =False)
+    brand_logo = db.Column(db.String(255), nullable =False)
+    
+    #relationships 
+    product = db.relationship('Product', back_populates = 'brands')
+    
+class Invoice (db.Model, SerializerMixin):
+    __tablename__ = 'invoices'
+    
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable = False)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable = False)
+    quantity = db.Column(db.Integer, nullable = False)
+    cost = db.Column(db.Integer, nullable = False)
+    created_at = db.Column(db.DateTime, default = datetime.now)
+    
+    #relationships
+    users = db.relationship('User', back_populates = 'invoice')
+    products = db.relationship('Product', back_populates = 'invoice')
+    invoice_products = db.relationship('InvoiceProducts', back_populates = 'invoice_rl')
+    
+
+class InvoiceProducts(db.Model, SerializerMixin):
+    __tablename__ = 'invoice_products'
+    id = db.Column(db.Integer, primary_key = True)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable = False)
+    invoice_id = db.Column(db.Integer, db.ForeignKey('invoices.id'), nullable = False)
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+    
+    #relationships 
+    product_rl = db.relationship('Product', back_populates = 'invoice_products')
+    invoice_rl = db.relationship('Invoice', back_populates = 'invoice_products')
+    
